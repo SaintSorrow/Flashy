d1287426a95c5f4d349f9d5272ef88c7fccd1891
Deimantas Gedgaudas <gedgaudasdei@gmail.com>
2019-11-03 16:05:34 +0100

    Add several different components

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import decks from './flashcards'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      showDeckList: true,
       showCards: false,
       incorrectCards: [],
       correctCards: [],
@@ -14,11 +13,21 @@ export default class App extends Component {
     }
   }
 
+  openDeck = (deck) => {
+    this.setState({
+      showDeckList: !this.state.showDeckList,
+      showCards: !this.state.showCards,
+      currentCards: deck.cards,
+    })
+  }
+
   render() {
     return (
       <View style ={styles.container}>
         {this.state.showDeckList === true && (
-          <DeckList decks={decks}/>
+          <DeckList decks={decks}
+          handler = {this.openDeck}
+          />
         )}
       </View>
     );
@@ -27,14 +36,28 @@ export default class App extends Component {
 
 function DeckComponent({ deck }) {
   return (
-    <View>
-      <Text style={styles.deckText}>{deck.name}</Text>
+    <View style={styles.deckComponent}>
+      <TouchableOpacity style={styles.deckComponent} onPress = {this.props.handler(deck)}>
+        <Text style={styles.deckText}>{deck.name}</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
+class DeckComponent extends Component {
+
+}
+
+class DeckList extends Component {
+  render() {
+    return (
+      {this.props.decks.map((deck) => )}
+    )
+  }
+}
+
 function DeckList({ decks }) {
-  const list = decks.map((deck) => <DeckComponent deck={deck} key={deck.name}/>)
+  const list = decks.map((deck) => <DeckComponent deck={deck} key={deck.name} handler={this.props.handler}/>)
 
   return (
     <View>
@@ -48,11 +71,18 @@ function DeckList({ decks }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+
+    backgroundColor: '#1adbb8',
     alignItems: 'center',
     justifyContent: 'center',
   },
   deckText: {
     color: 'red',
   },
+  deckComponent: {
+    backgroundColor: '#2604bf',
+    marginTop: 5,
+    marginBottom: 5,
+    padding: 5,
+  }
 });
